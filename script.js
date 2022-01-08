fc2cce270231b30892102503a448410337e063bd
Piero <pieroq.2899@gmail.com>
2022-01-08 18:35:01 -0500

    Add buttons for user input

---
+++ script.js
@@ -0,0 +1,112 @@
+function computerPlay()
+{
+    const min = 1;
+    const max = 3;
+    const play = Math.floor(Math.random() * (max - min +1)) + min;
+
+    switch (play)
+    {
+        case 1: 
+        return "Rock"; 
+        break;
+
+        case 2: 
+        return "Paper"; 
+        break;
+
+        case 3: 
+        return "Scissors";
+        break; 
+    }
+}
+
+
+function round(playerSelection)
+{
+    let computerSelection = computerPlay();
+    //let playerSelection = prompt("Select Rock, Paper or Scissors","");
+    //playerSelection = playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1);
+
+   switch (playerSelection)
+   {
+       case "Rock":
+           switch (computerSelection)
+           {
+               case "Rock": 
+               return "It's a tie!";
+               break;
+               case "Paper": 
+               return "You lose! Paper beats Rock";
+               break;
+               case "Scissors": 
+               return "You win! Rock beats Scissors"; 
+           }
+       break;
+       case "Paper":
+       switch (computerSelection)
+           {
+               case "Rock": 
+               return "You win! Paper beats Rock" ;
+               break;
+               case "Paper": 
+               return "It's a tie!";
+               break;
+               case "Scissors": 
+               return "You lose! Scissors beat Paper" ;
+           } 
+       break;
+       case "Scissors": 
+       switch (computerSelection)
+           {
+               case "Rock": 
+               return "You lose! Rock beats Scissors";
+               break;
+               case "Paper": 
+               return "You win! Scissors beat Paper";
+               break;
+               case "Scissors": 
+               return "It's a tie!"; 
+           }
+       break;
+       default:
+       return "Invalid input"; 
+   }
+}
+/*
+function game()
+{
+    
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+while (rounds<5)
+{
+    let outcome = round();
+    console.log(outcome);
+
+    if (outcome.includes("win")) playerScore++;
+    else if (outcome.includes("lose")) computerScore++;
+
+    rounds++;
+}
+
+if (playerScore > computerScore) 
+    console.log(`---You won this game! ${playerScore}-${computerScore}---`);
+else if (playerScore < computerScore) 
+    console.log(`---You lost this game! ${playerScore}-${computerScore}---`);
+else
+    console.log(`---This game was a tie! ${playerScore}-${computerScore}---`); 
+
+}
+
+game()*/
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        console.log(round(button.textContent));
+    })
+});
+
